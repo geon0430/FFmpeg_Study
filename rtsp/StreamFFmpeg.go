ddc6dfcf8132d6f29698df8bd6a85da2af65836a
Geon <114966864+geon0430@users.noreply.github.com>
2024-02-16 09:36:31 +0900

    Update StreamFFmpeg.go

--- rtsp/StreamFFmpeg.go
+++ rtsp/StreamFFmpeg.go
@@ -10,7 +10,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func FFmpegStream(
+func GPUFFmpegStream(
 	gpuDevice int,
 	pipelineConfig util.PipelineInfo,
 	videoWidth, videoHeight int,
@@ -67,3 +67,57 @@ func FFmpegStream(
 	return cmd, stdin, nil
 }
 
+
+func CPUFFmpegStream(
+	pipelineConfig util.PipelineInfo,
+	videoWidth, videoHeight int,
+	streamUrl string,
+	logger *logrus.Logger,
+	errorChan chan<- error,
+) (*exec.Cmd, io.WriteCloser, error) {
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	FPS := pipelineConfig.RtspInfo.FPS
+	// encoder := pipelineConfig.RtspInfo.ENCODER
+
+	cmd := exec.Command("ffmpeg",
+		"-re",
+		"-f", "rawvideo",
+		"-pixel_format", "bgr24",
+		"-video_size", fmt.Sprintf("%dx%d", videoWidth, videoHeight),
+		"-framerate", fmt.Sprintf("%f", FPS),
+		"-i", "-",
+		"-timeout", "300000",
+		"-reconnect", "1",
+		"-reconnect_streamed", "1",
+		"-reconnect_delay_max", "300",
+		"-vf", fmt.Sprintf("scale=%d:%d", videoWidth, videoHeight),
+		"-c:v", "libx264",
+		"-preset", "fast",
+		"-maxrate", "4000k",
+		"-g", "60",
+		"-f", "rtsp",
+		"-rtsp_transport", "tcp",
+		fmt.Sprintf(streamUrl),
+	)
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		logger.Errorf("Error creating stdin pipde: %v", err)
+		errorChan <- err
+		return nil, nil, err
+	}
+
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	if err := cmd.Start(); err != nil {
+		logger.Errorf("Error starting command: %v", err)
+		errorChan <- err
+		return nil, nil, err
+	}
+
+	logger.Infof("Streaming command started successfully")
+	return cmd, stdin, nil
+}
